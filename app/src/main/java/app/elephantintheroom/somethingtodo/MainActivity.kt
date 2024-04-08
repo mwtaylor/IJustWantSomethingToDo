b8fb835b8e3f40c59e6a4b7155f3ce990769d13a
Michael Taylor <mwtaylor@users.noreply.github.com>
2024-04-07 21:02:14 -0700

    Create an Android Studio project

---
+++ app/src/main/java/app/elephantintheroom/somethingtodo/MainActivity.kt
@@ -0,0 +1,46 @@
+package app.elephantintheroom.somethingtodo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.elephantintheroom.somethingtodo.ui.theme.IJustWantSomethingToDoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IJustWantSomethingToDoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IJustWantSomethingToDoTheme {
+        Greeting("Android")
+    }
+}
