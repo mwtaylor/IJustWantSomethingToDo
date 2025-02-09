6bf6fb195f3c08623ee9087395725991fddebfcf
Michael Taylor <mwtaylor@users.noreply.github.com>
2025-02-08 19:49:35 -0800

    Focus on name field when adding a new thing to do

--- screens/thingstodo/src/main/java/app/elephantintheroom/ijustwantsomethingtodo/screens/thingstodo/ui/NewThingToDoPane.kt
+++ screens/thingstodo/src/main/java/app/elephantintheroom/ijustwantsomethingtodo/screens/thingstodo/ui/NewThingToDoPane.kt
@@ -16,12 +16,15 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -63,12 +66,18 @@ fun NewThingToDoPane(
             }
 
             var name by remember { mutableStateOf("") }
+            val focusRequester = remember { FocusRequester() }
             OutlinedTextField(
                 value = name,
                 onValueChange = { name = it },
                 label = { Text("Name") },
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester),
             )
+            LaunchedEffect(Unit) {
+                focusRequester.requestFocus()
+            }
 
             FilledIconButton(
                 onClick = { save(ThingToDo(null, name)) },
